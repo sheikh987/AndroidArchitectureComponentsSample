9d25d1c9fbe16e4aeb5aa1624773eda3836bcebb
sheikh <chishti36@gmail.com>
2018-04-20 10:24:24 +0530

    creating entity Word

---
+++ app/src/main/java/com/example/sheikh/androidarchitecturecomponentssample/Word.java
@@ -0,0 +1,25 @@
+package com.example.sheikh.androidarchitecturecomponentssample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by sheikh on 4/20/2018.
+ */
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord() {return this.mWord;}
+
+
+}
